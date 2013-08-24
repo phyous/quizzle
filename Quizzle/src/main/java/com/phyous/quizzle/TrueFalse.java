1b137ff6199c53f2d1e3675081174d97641cfe29
Philip Youssef <pyoussef@twitter.com>
2013-08-24 15:53:09 -0700

    first commit

---
+++ Quizzle/src/main/java/com/phyous/quizzle/TrueFalse.java
@@ -0,0 +1,28 @@
+package com.phyous.quizzle;
+
+public class TrueFalse {
+    private int mQuestion;
+    private boolean mTrueQuestion;
+
+    public TrueFalse(int question, boolean trueQuestion) {
+        mQuestion = question;
+        mTrueQuestion = trueQuestion;
+
+    }
+
+    public int getQuestion() {
+        return mQuestion;
+    }
+
+    public boolean isTrueQuestion() {
+        return mTrueQuestion;
+    }
+
+    public void setTrueQuestion(boolean trueQuestion) {
+        mTrueQuestion = trueQuestion;
+    }
+
+    public void setQuestion(int question) {
+        mQuestion = question;
+    }
+}
